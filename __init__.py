12eab4680b8bfd65a28e2cfaa3ac36b56c77c7dd
Alexander Bliskovsky <abliskovsky@fusionbox.com>
2015-05-19 13:24:18 -0600

    Add mixin function.

---
+++ __init__.py
@@ -0,0 +1,28 @@
+def DecoratorMixin(decorator):
+    """
+    Converts a decorator written for a function view into a mixin for a
+    class-based view.
+
+    ::
+
+        LoginRequiredMixin = DecoratorMixin(login_required)
+
+        class MyView(LoginRequiredMixin):
+            pass
+
+        class SomeView(DecoratorMixin(some_decorator),
+                       DecoratorMixin(something_else)):
+            pass
+
+    """
+
+    class Mixin(object):
+        __doc__ = decorator.__doc__
+
+        @classmethod
+        def as_view(cls, *args, **kwargs):
+            view = super(Mixin, cls).as_view(*args, **kwargs)
+            return decorator(view)
+
+    Mixin.__name__ = str('DecoratorMixin(%s)' % decorator.__name__)
+    return Mixin
